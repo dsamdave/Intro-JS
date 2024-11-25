6686a0ae8bf3dce0edc632f9c1e9bd19647f43bb
David Sampson <Davidchosen33@gmail.com>
2024-11-25 18:11:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,162 @@
+
+
+// const firstName = "David"
+
+// const lastName = "Sampson"
+
+// const locationn = "Port Harcourt"
+
+
+
+// console.log(firstName.length)
+
+// console.log(firstName.includes("D"))
+
+// console.log(lastName.endsWith("z"))
+
+// console.log(firstName.trim())
+// console.log(firstName)
+
+// console.log(firstName.toLowerCase())
+// console.log(firstName.toUpperCase())
+
+// console.log(firstName.indexOf("i"))
+
+
+// const message = "my name is " + firstName + " " + lastName + " I am from " + locationn
+
+// const messagee = `My name is ${firstName} ${lastName} . I am from ${loca tionn}`
+
+// console.log(message)
+
+// const gg = 12
+
+// const ff = 24
+
+// console.log(gg + ff)
+
+// const a = 3
+
+// const b = 6
+
+// const c = 4
+
+// const d = 10
+
+// const answer = 2 * (a ** b) - 10 / d
+
+// console.log(answer)
+
+// const x = 2
+// const y = 3
+
+// const ans = (2 * (x + y)) / 3
+
+// console.log(ans)
+
+// // Math.floor()
+// const yy = Math.random() 
+// const xx = Math.floor(yy)
+
+// console.log(xx)
+
+
+
+// const aa = 
+// const bb = 
+
+
+// const jssOne = [ "David",  "Sampson"]
+
+// const scores = [ 10, 23, 69, 66, 55 ]
+// const hh = [ "HGfff", 222, 55, false ]
+
+// console.log(jssOne)
+
+// jssOne.push("tjdcjtghftg")
+
+// console.log(jssOne)
+
+// jssOne.pop()
+// jssOne.pop()
+
+// console.log(jssOne)
+
+// jssOne.unshift("GGGGGG")
+
+// console.log(jssOne)
+
+// const y = jssOne.includes("Sampson")
+// console.log(y)
+
+
+// const laptop = {
+//     model: "HP",
+//     color: "Black",
+//     os: "Windows",
+//     price: 12000,
+//     isMarried: false,
+//     paid: null,
+//     loactions: [
+//         "PH", 
+//         "Lagos", 
+//         "ABJ",
+//         {gg: false, jj: true, ll: null}
+//     ],
+//     sizes: {
+//         small: 12,
+//         md: 77,
+//         lg: 1789
+//     }
+// }
+// console.log(laptop.loactions[3].jj)
+// console.log(laptop.loactions[0])
+
+// const hh = 
+
+// const yy = 
+
+// const ff = ["fgyugb", "jyfvyhjgybjh" ]
+
+// const tt = [12, 5, 78, 90]
+
+// const uu = [12, true, null, "yufvhgg"]
+
+// console.log(ff)
+
+// ff.push("Ddddd")
+
+// console.log(ff)
+
+// ff.pop()
+
+// console.log(ff)
+
+// const person = { 
+//     firstName: "",jss
+//     lastName: "",
+//     age: 12,
+//     isMarried: false,
+//     grand: null,
+//     children: ["jj", "kk", "ll"]
+// }
+
+// console.log(person.children[1])
+
+// Loops
+
+const jssTwo = ["Cy", "Jnr", "CJ", "KC"]
+
+// let x = 0
+
+// while( x < 3 ){
+
+//     console.log(jssTwo[x])
+
+//     x++
+
+// }
+
+for(let y = 1; y <= jssTwo.length; y++){
+    console.log(jssTwo[y])
+}
